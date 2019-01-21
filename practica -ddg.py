0a99404340450909da3d5fee1a0164ffcf0766a6
Carol <cbertoncinifernandez@gmail.com>
2019-01-21 16:16:11 +0100

    Mi commit

---
+++ practica -ddg.py
@@ -0,0 +1,8 @@
+jugador = int(input("Introduzca un número: "))
+
+def ddg(jugador):
+    jugadores = ["Juan", "Jose", "Maria", "Pedro"]
+    for i in range(len(jugadores)):
+        if i == jugador:
+            return (jugadores[i])
+print(ddg(jugador))
